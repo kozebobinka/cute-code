189d3427651ce9e68f54e04e79d049ce34d3fb72
Olena Sinelnyk <kozebobinka@gmail.com>
2023-01-13 18:44:49 +0200

    cs fixer

--- src/Exchanger/ApilayerExchanger.php
+++ src/Exchanger/ApilayerExchanger.php
@@ -1,7 +1,8 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace CuteCode\Exchanger;
-
 
 use CuteCode\Exchanger\Client\ExchangerClientInterface;
 use CuteCode\Exchanger\DTO\RatesDTO;
